fc4067fcdc24020a5017a3b07afc73f41acc9b6c
Kenhubhub <98609221+Kenhubhub@users.noreply.github.com>
2022-11-30 09:18:36 +0000

    Program to interact with university api

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+// [24/11 10:56] Sujith Fernando
+// 1 .Create a .Net Solution (c# language) 
+
+// [24/11 10:57] Sujith Fernando
+// 2.Create a class library that can talk to this API and returns list of universities per country 
+
+// [24/11 10:59] Sujith Fernando
+// 3. Create a Console  Application that can talk to the new class library you created and display Universities information in any format that you think of it 
+
+
+// App to find Uni by name and then return a website
+    // Use LINQ library to filter by name
+
+namespace UniApp // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var api = new UniApi();
+            await api.getUnis("united kingdom");
+        }
+    }
+}
---
+++ UniApi.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using System.Net.Http;
+using Newtonsoft.Json;
+namespace UniApp{
+    class UniApi{
+        
+        public async Task getUnis(string country){
+            HttpClient client = new HttpClient();
+            string urlCountry = country.Replace(" ", "+");
+            Console.WriteLine(urlCountry);
+            string url = $"http://universities.hipolabs.com/search?country={urlCountry}";
+            Console.WriteLine(url);
+            string rawdata = await client.GetStringAsync("http://universities.hipolabs.com/search?country=United+Kingdom");
+            this.universities = JsonConvert.DeserializeObject<List<Universities>>(rawdata);
+        }
+        List <Universities> universities;
+    }
+    class Universities{
+        public string country;
+        public string[] domains;
+        public string[] web_pages;
+        public string alpha_two_code;
+        public string name;
+        public string state;
+    }
+}
